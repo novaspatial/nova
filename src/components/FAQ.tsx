ca1084ae82e49f5537955196cade1e70c3fdb811
Onur <onur.onel2230@gmail.com>
2026-02-16 17:51:49 -0500

    feat: add FAQ section

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/react'
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
+
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+import { SectionIntro } from '@/components/SectionIntro'
+
+const faqs = [
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aut tempora vitae odio inventore fuga aliquam nostrum quod porro. Delectus quia facere id sequi expedita natus.',
+  },
+  {
+    question: 'What do you call someone with no body and no nose?',
+    answer:
+      'Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas ipsa quia excepturi, quibusdam natus exercitationem sapiente tempore labore voluptatem.',
+  },
+  {
+    question: 'Why do you never see elephants hiding in trees?',
+    answer:
+      "Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "Why can't you hear a pterodactyl go to the bathroom?",
+    answer:
+      'Because the pee is silent. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, quas voluptatibus ex culpa ipsum, aspernatur blanditiis fugiat ullam magnam suscipit deserunt illum natus facilis atque vero consequatur! Quisquam, debitis error.',
+  },
+  {
+    question: 'Why did the invisible man turn down the job offer?',
+    answer:
+      "He couldn't see himself doing it. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet perspiciatis officiis corrupti tenetur. Temporibus ut voluptatibus, perferendis sed unde rerum deserunt eius.",
+  },
+]
+
+export function FAQ() {
+  return (
+    <div className="mt-24 sm:mt-32 lg:mt-40">
+      <SectionIntro
+        eyebrow="FAQ"
+        title="Frequently asked questions"
+      >
+        <p>
+          Can&apos;t find the answer you&apos;re looking for? Feel free to reach
+          out to our team.
+        </p>
+      </SectionIntro>
+
+      <Container className="mt-16">
+        <FadeIn>
+          <dl className="divide-y divide-neutral-950/10">
+            {faqs.map((faq) => (
+              <Disclosure
+                key={faq.question}
+                as="div"
+                className="py-6 first:pt-0 last:pb-0"
+              >
+                <dt>
+                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-neutral-950">
+                    <span className="text-base/7 font-semibold">
+                      {faq.question}
+                    </span>
+                    <span className="ml-6 flex h-7 items-center">
+                      <PlusSmallIcon
+                        aria-hidden="true"
+                        className="size-6 group-data-open:hidden"
+                      />
+                      <MinusSmallIcon
+                        aria-hidden="true"
+                        className="size-6 group-[&:not([data-open])]:hidden"
+                      />
+                    </span>
+                  </DisclosureButton>
+                </dt>
+                <DisclosurePanel as="dd" className="mt-2 pr-12">
+                  <p className="text-base/7 text-neutral-600">
+                    {faq.answer}
+                  </p>
+                </DisclosurePanel>
+              </Disclosure>
+            ))}
+          </dl>
+        </FadeIn>
+      </Container>
+    </div>
+  )
+}
