525b397ffb838ac934a79e1a19cee0c71f67f428
Onur <6f6e7572@pm.me>
2026-02-17 13:10:42 -0500

    ci: configure Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ import { unifiedConditional } from 'unified-conditional'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/nova',
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
