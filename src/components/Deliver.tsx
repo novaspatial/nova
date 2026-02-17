b3a0a75bb0594ad334789331299a9d4cebc6cd2e
Onur <6f6e7572@pm.me>
2026-02-17 14:28:30 -0500

    style: update color palette

--- src/components/Deliver.tsx
+++ src/components/Deliver.tsx
@@ -42,14 +42,9 @@ export function Deliver() {
         <ListItem title="Testing">
           Our projects always have 100% test coverage, which would be impressive
           if our tests weren't as porous as a sieve.
-        </ListItem>
-        <ListItem title="Infrastructure">
+
           To ensure reliability we only use the best Digital Ocean droplets that
           $4 a month can buy.
-        </ListItem>
-        <ListItem title="Support">
-          Because we hold the API keys for every critical service your business
-          uses, you can expect a lifetime of support, and invoices, from us.
         </ListItem>
       </List>
     </Section>
