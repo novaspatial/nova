76543275320241356939c37232a300d99ce6e92b
Onur <6f6e7572@pm.me>
2026-02-27 19:20:54 -0500

    perf: optimize image and font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { type Metadata } from 'next'
+import localFont from 'next/font/local'
 
 import '@/styles/tailwind.css'
+
+const monaSans = localFont({
+  src: '../fonts/Mona-Sans.var.woff2',
+  display: 'swap',
+  variable: '--font-mona-sans',
+  weight: '200 900',
+  style: 'normal',
+})
 
 export const metadata: Metadata = {
   title: {
@@ -11,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full bg-neutral-950 text-base antialiased" suppressHydrationWarning>
+    <html lang="en" className={`h-full bg-zinc-950 text-base antialiased ${monaSans.variable}`} suppressHydrationWarning>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   )
